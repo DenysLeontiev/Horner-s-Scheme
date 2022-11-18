1884cf9b2bddb7b10994e1bcda7d2e88095b0f72
DenLeon <leonden0046@gmail.com>
2022-11-18 15:07:56 +0200

    Changes after lesson aplied

--- Laba3/Source.cpp
+++ Laba3/Source.cpp
@@ -5,61 +5,95 @@ using namespace std;
 
 int setValue(const char[]);
 int* generateArray(int);
+int getResult(int* generatedArr, int arraySize, int x);
+int getDeritative(int* arr, int size, double x);
 
 void main()
 {
-    int x = setValue("x");
-    int arraySize = setValue("size of array");
+	int x = setValue("x");
+	int arraySize = setValue("size of array");
 
-    int* generatedArray = generateArray(arraySize);
+	int* generatedArray = generateArray(arraySize);
 
-    int result = generatedArray[0] * x + generatedArray[1];
+	int result = getResult(generatedArray, arraySize, x);
 
-    for (int i = 2; i < arraySize; i++)
-    {
-        result *= x;
-        result += generatedArray[i];
-    }
+	/*int result = generatedArray[0];
 
-    
-    cout << result;
+	for (int i = 0; i < arraySize; i++)
+	{
+		result *= x;
+		result += generatedArray[i];
+	}*/
+
+
+	cout << "result = "<< result << endl;
+
+	int r = getDeritative(generatedArray, arraySize, x);
+	cout << "deritative =" << r;
+
+	system("pause");
 }
 
 int setValue(const char v[])
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int error;
-    int value;
-    do
-    {
-        error = 0;
-        cout << "Please enter " << v << " = ";
-        cin >> value;
-        if (cin.fail())
-        {
-            cout.clear();
-            cout << "Please enter a valid number" << endl;
-            error = 1;
-            cin.clear();
-            cin.ignore(80, '\n');
-        }
-    } while (error == 1);
+	int error;
+	int value;
+	do
+	{
+		error = 0;
+		cout << "Please enter " << v << " = ";
+		cin >> value;
+		if (cin.fail())
+		{
+			cout.clear();
+			cout << "Please enter a valid number" << endl;
+			error = 1;
+			cin.clear();
+			cin.ignore(80, '\n');
+		}
+	} while (error == 1);
 
-    return value;
+	return value;
 }
 
 int* generateArray(int arrSize)
 {
-    int* array = new int [arrSize];
+	int* array = new int[arrSize];
 
-    for (int i = 0; i < arrSize; i++)
-    {
-        cout << "Enter " << "N." << (i + 1) << " element = ";
-        int value = 0;
-        cin >> value;
-        array[i] = value;
-    }
+	for (int i = 0; i < arrSize; i++)
+	{
+		cout << "Enter " << "N." << (i + 1) << " element = ";
+		int value = 0;
+		cin >> value;
+		array[i] = value;
+	}
 
-    return array;
+	return array;
+}
+
+int getResult(int* generatedArr, int arraySize, int x)
+{
+	int result = generatedArr[0];
+
+	for (int i = 0; i < arraySize; i++)
+	{
+		result *= x;
+		result += generatedArr[i];
+	}
+
+	return result;
+}
+
+int getDeritative(int* arr, int size, double x)
+{
+	int res = 1;
+
+	for (int i = 1; i < size; i++)
+	{
+		res *= x * (size - i) * arr[i];
+	}
+
+	return res;
 }
